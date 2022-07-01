0d8114ef20152030f06fbf84ebb3e7b6a694c0ba
Fedisco067 <fedisco067@gmail.com>
2022-06-30 18:09:46 -0700

    Task 4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <studio.h>
-/**
- * main -Entry point
- *
- * Return: Always 0 (success)
- */
+/
+* main -Entry point
+* Return: Always 0(success)
+*/
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building multilingual puzzle");
 	return (0);
 }
