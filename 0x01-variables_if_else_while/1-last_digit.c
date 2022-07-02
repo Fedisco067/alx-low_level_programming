ced3924bc8f08121086e1fed891b3985f3d9a43d
Fedisco067 <fedisco067@gmail.com>
2022-07-02 15:27:10 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 int n;
+int d;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
@@ -27,5 +28,4 @@ else if (d < 6 && d != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
 }
 return (0);
-
-}  
+}
