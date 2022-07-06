11330a98d7e06bd90c47fe52ec9339eb5876e5e6
Fedisco067 <fedisco067@gmail.com>
2022-07-05 20:14:54 -0700

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_×10(void);
+void print_alphabe_10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -11,5 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
 void print_times_table(int n);
+void num(int r, int n);
 #endif
