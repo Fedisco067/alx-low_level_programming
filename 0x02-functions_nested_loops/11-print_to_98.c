b4d2179a9c92d15836495a9eac52f43df551037f
Fedisco067 <fedisco067@gmail.com>
2022-07-07 19:38:32 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 -prints all natural numbers from n to 98, followed by a new line
+ * @n: first number to print
+ * Return: Always 0
+ */
+void print_to_98(int n) 
+
+{
+_putchar(0);
+_putchar(n++);
+_putchar(98);
+_putchar('\n');
+return (0);
+}
